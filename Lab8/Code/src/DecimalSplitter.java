00ed4c9314de1bc7d57b0e69cc64c0d9cb44c112
Akshay Kotak <40596462+akshay2102@users.noreply.github.com>
2020-07-16 21:23:22 +0530

    Completed Lab8

--- Lab8/Code/src/DecimalSplitter.java
+++ Lab8/Code/src/DecimalSplitter.java
@@ -1,21 +1,16 @@
 public class DecimalSplitter {
 	static boolean isOdd(int num) {
 //Complete the logic
+		return num%2==1;
 	}
 	static int getWhole(double num) {
 //Complete the logic
+		return (int)num;
 	}
-	
 	static double getFraction(double num) {
 //Complete the logic
+		String ds = String.valueOf(num);
+		int index = ds.indexOf(".");
+		return Double.valueOf(ds.substring(index));
 	}
 }
-
-
-
-
-
-
-
-
-
--- Lab8/Code/src/TestMain.java
+++ Lab8/Code/src/TestMain.java
@@ -5,11 +5,10 @@ public class TestMain {
 			System.exit(0);
 		}
 		
-		double num = 0;
-		int whole = 0;
-		double fraction = 0;
+		double num = Double.parseDouble(args[0]);
+		int whole = DecimalSplitter.getWhole(num);
+		double fraction = DecimalSplitter.getFraction(num);
 		
-
 // complete the statements
 
 		System.out.println("Whole : " + whole);
