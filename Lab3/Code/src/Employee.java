df77dbcd58bad4eb74c5e183f61636a0e1640536
Akshay Kotak <40596462+akshay2102@users.noreply.github.com>
2020-07-16 17:05:16 +0530

    Completed Lab3

--- Lab3/Code/src/Employee.java
+++ Lab3/Code/src/Employee.java
@@ -1,5 +1,25 @@
 public class Employee {
-
 // Identity variables and methods. Create association with Address
-
+	private String empID;
+	private String empName;
+	private Address addr;
+	
+	public String getEmpID() {
+		return empID;
+	}
+	public void setEmpID(String empID) {
+		this.empID = empID;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public Address getAddr() {
+		return addr;
+	}
+	public void setAddr(Address addr) {
+		this.addr = addr;
+	}
 }
