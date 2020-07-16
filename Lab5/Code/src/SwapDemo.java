d413364fcc5a937fc0692567533a5fe2f604563e
Akshay Kotak <40596462+akshay2102@users.noreply.github.com>
2020-07-16 18:10:15 +0530

    Completed Lab5

--- Lab5/Code/src/SwapDemo.java
+++ Lab5/Code/src/SwapDemo.java
@@ -2,15 +2,19 @@ public class SwapDemo {
 	public static void main(String[] args) {
 		int arg1 = 100;
 		int arg2 = 200;
-	
+		
 	// make call to swapValues() and displayValues()
-
+		displayValues("Before Swapping: ",arg1,arg2);
+		swapValues(arg1,arg2);
+		displayValues("\nAfter Swapping: ",arg1,arg2);
 	}
 
 	private static void swapValues(int arg1,int arg2) {
-	
 	// Write logic to swap values
-
+		arg1 = arg1 + arg2;
+		arg2 = arg1 - arg2;
+		arg1 = arg1 - arg2;
+		// displayValues("\nInside Swap",arg1,arg2);
 	}
 	
 	private static void displayValues(String str,int arg1, int arg2) {
