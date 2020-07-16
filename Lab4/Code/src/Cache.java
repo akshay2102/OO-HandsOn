3230a6d46f5f6c825fa02aba1356c788f9cbb61b
Akshay Kotak <40596462+akshay2102@users.noreply.github.com>
2020-07-16 17:27:30 +0530

    Completed Lab4

--- Lab4/Code/src/Cache.java
+++ Lab4/Code/src/Cache.java
@@ -2,7 +2,12 @@ public class Cache {
 	private static int MAX_CAPACITY = 0;
 	
 	// Implement static block
-
+	static {
+		while(MAX_CAPACITY<1) {
+			System.out.println("Enter Max Capacity: ");
+			MAX_CAPACITY = Console.readInt();
+		}
+	}
 	static int getMaxCapacity() {
 		System.out.println("Returning MAX_CAPACITY");
 		return MAX_CAPACITY;
